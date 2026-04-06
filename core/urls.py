fb1ab8815639ef9445eec1e7160169d4c672e747
Yandisa <gaju.yandisa@gmail.com>
2026-04-06 20:29:04 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from website.sitemaps import StaticSitemap, FixtureSitemap
+
+sitemaps = {
+    'static': StaticSitemap,
+    'fixtures': FixtureSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
